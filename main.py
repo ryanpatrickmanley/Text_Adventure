14243670b406aa17c8d3f2e1eb8b4eb13d81f2cd
ryanpatrickmanl <21130828-ryanpatrickmanl@users.noreply.replit.com>
2023-04-09 05:24:21 +0000

    succeeded in creating a stat tracking module! w00t!

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import scenes
 import stat
 
-# Declare some VARIABLES baby!
-global job = 0
-global flowers_smelled = False
-
 ##############################################################################
 
-job = scenes.intro()
+# Intro.
+stat.job = scenes.intro()
 
-# Intro, cliffside.
-flowers_smelled = scenes.cliffside(job)
+# Cliffside.
+stat.flowers_smelled = scenes.cliffside(stat.job)
